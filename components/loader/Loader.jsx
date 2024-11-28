1076ec0572fadf75d0585b2bfaea284300555bc7
shhady <shhadyse@gmail.com>
2024-11-28 12:56:24 +0200

    urls

--- components/loader/Loader.jsx
+++ components/loader/Loader.jsx
@@ -2,7 +2,7 @@ import './Loader.css';
 export default function Loader() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h2 className="loaderText"></h2>
+        {/* <h2 className="loaderText"></h2> */}
         <div className="loader"></div>
       </div>
     );
