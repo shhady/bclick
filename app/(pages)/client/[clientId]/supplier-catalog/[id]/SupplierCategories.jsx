9430777988158f6fc88fa36cb697e1fb2288a061
shhady <shhadyse@gmail.com>
2024-12-04 19:33:30 +0200

    loader

--- app/(pages)/client/[clientId]/supplier-catalog/[id]/SupplierCategories.jsx
+++ app/(pages)/client/[clientId]/supplier-catalog/[id]/SupplierCategories.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 export default function SupplierCategories({ serializedCategories, onCategoryClick }) {
+    
+const sortedCategories = [...serializedCategories].sort((a, b) => {
+    if (a.name === 'כללי') return -1; // Move 'כללי' to the beginning
+    if (b.name === 'כללי') return 1;
+    return 0;
+  });
+
   return (
     <div className="bg-gray-400 h-[35px] lg:h-[50px] flex items-center overflow-x-auto whitespace-nowrap">
       <div className="flex items-center gap-4 px-4">
-        {serializedCategories?.map((category) => (
+        {sortedCategories?.map((category) => (
           <button
             key={category._id}
             onClick={() => onCategoryClick(category._id)}
