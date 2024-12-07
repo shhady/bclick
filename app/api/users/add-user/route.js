641efcbce1b62b5b035c45f83e1a9258fe9d2cf7
shhady <shhadyse@gmail.com>
2024-12-07 16:19:50 +0200

    check add to fav

--- app/api/users/add-user/route.js
+++ app/api/users/add-user/route.js
@@ -1,5 +1,3 @@
-import initializeClientNumberCounter from '@/utils/initializeClientNumberCounter';
-import getNextClientNumber from '@/utils/getNextClientNumber';
 import User from '@/models/user';
 import { connectToDB } from '@/utils/database';
 
@@ -7,19 +5,25 @@ export async function POST(req) {
     try {
         const data = await req.json();
         console.log(data);
+
         // Connect to the database
         await connectToDB();
 
-        // Ensure the counter is synced with the highest client number
-        await initializeClientNumberCounter();
+        // Fetch the last user with the highest clientNumber
+        const lastUser = await User.findOne()
+            .sort({ clientNumber: -1 }) // Sort by clientNumber descending
+            .collation({ locale: "en", numericOrdering: true }) // Ensure numeric sorting
+            .lean();
 
-        // Get the next client number
-        const clientNumber = await getNextClientNumber();
-        
+        const nextClientNumber = lastUser?.clientNumber ? parseInt(lastUser.clientNumber) + 1 : 1;
+
+        console.log('Last User:', lastUser);
+        console.log('Next Client Number:', nextClientNumber);
+
         // Create a new user with the assigned client number
         const newUser = await User.create({
             ...data,
-            clientNumber,
+            clientNumber: nextClientNumber,
         });
 
         return new Response(JSON.stringify(newUser), { status: 201 });
