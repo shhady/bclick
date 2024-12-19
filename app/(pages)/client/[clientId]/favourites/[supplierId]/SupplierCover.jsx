8ad1a41ff51c98b92426860886a27a5c4695bd43
shhady <shhadyse@gmail.com>
2024-12-19 12:23:42 +0200

    orders notification

--- app/(pages)/client/[clientId]/favourites/[supplierId]/SupplierCover.jsx
+++ app/(pages)/client/[clientId]/favourites/[supplierId]/SupplierCover.jsx
@@ -1,30 +1,22 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from 'next/image'
+import React from 'react'
 
-export default function SupplierCover({ supplier }) {
-  const imageUrl = supplier?.coverImage?.secure_url
-    ? `${supplier.coverImage.secure_url}`
-    : null;
-
+export default function SupplierCover({supplier}) {
   return (
-    <div className="h-50 lg:h-72 bg-customBlue rounded-b-xl">
-      {imageUrl ? (
-        <Image
-          src={imageUrl}
-          alt={supplier.name ? `${supplier.name} Cover Image` : 'Supplier Cover Image'}
-          width={500}
-          height={500}
-          priority // Ensures early loading for better LCP
-          placeholder="blur"
-          blurDataURL={supplier.coverImage.blurDataURL || "data:image/jpeg;base64,..."} // Optimized placeholder
-          className="object-cover w-full h-full rounded-b-xl"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" // Responsive sizes
-        />
-      ) : (
-        <div className="flex items-center justify-center h-full text-white bg-gray-400">
-          No Cover Image Available
+    <div>
+        {supplier?.coverImage && (
+        <div className='h-50 lg:h-72 bg-customBlue rounded-b-xl '>
+          <Image
+            src={supplier?.coverImage?.secure_url}
+            width={500}
+            height={500}
+            alt='cover'
+            className='w-full h-full object-cover max-h-1/4'
+            priority
+            placeholder='blur'
+          />
         </div>
       )}
     </div>
-  );
+  )
 }
