d449b85b79b1fd235ccde451c622a74146dd2a9a
shhady <shhadyse@gmail.com>
2024-12-26 16:50:45 +0200

    catalog

--- app/(pages)/client/[clientId]/supplier-catalog/[id]/loading.js
+++ app/(pages)/client/[clientId]/supplier-catalog/[id]/loading.js
@@ -9,12 +9,12 @@ export default function Loading() {
       
       {/* Supplier Details Skeleton */}
       <div className="max-w-4xl mx-auto p-4">
-        <div className="flex items-center space-x-4 mb-6">
-          <div className="w-16 h-16 bg-gray-200 rounded-full"></div>
-          <div className="flex-1">
+        <div className="flex justify-between items-start space-x-4 mb-6">
+        <div className="flex-1">
             <div className="h-6 bg-gray-200 rounded w-1/3 mb-2"></div>
             <div className="h-4 bg-gray-200 rounded w-1/4"></div>
           </div>
+          <div className="w-16 h-16 bg-gray-200"></div>
         </div>
       </div>
 
