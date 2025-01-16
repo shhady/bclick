fbc9dad7be90ad1b415ee9e788d88fc6e95eab67
shhady <shhadyse@gmail.com>
2025-01-17 00:26:41 +0200

    spacing, some design

--- app/(pages)/supplier/catalog/update-product/[id]/EditProductClient.jsx
+++ app/(pages)/supplier/catalog/update-product/[id]/EditProductClient.jsx
@@ -335,7 +335,7 @@ export default function EditProductClient({ product, categories }) {
                 onClick={() => fileInputRef.current?.click()}
                 disabled={isUploading}
                 className={`${
-                  isUploading ? 'bg-gray-400' : 'bg-blue-500 hover:bg-hoveredBlue'
+                  isUploading ? 'bg-gray-400' : 'bg-customBlue hover:bg-hoveredBlue'
                 } text-white px-4 py-2 rounded-md w-2/4 text-center cursor-pointer`}
               >
                 {isUploading 
