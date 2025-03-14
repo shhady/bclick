aa13cc6876f782f62c09784eb03c2ad566780fad
shhady <shhadyse@gmail.com>
2025-03-14 23:44:25 +0200

    new first

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <div dir='ltr'className='h-[90vh]  w-full flex justify-center items-center'>
+  return <div dir='ltr'className='h-[90vh] mt-10  w-full flex justify-center items-center'>
      <SignUp fallbackRedirectUrl="/newprofile"/></div>;
 }
